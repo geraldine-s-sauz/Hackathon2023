cd1c970335ce3be0f07d90e1b7be96dc1924d40e
Edrian Camacho <edriangustin.camacho.20@usjr.edu.ph>
2023-07-26 00:23:40 +0800

    fix site.js

--- HackathonWeb/wwwroot/js/site.js
+++ HackathonWeb/wwwroot/js/site.js
@@ -3,19 +3,24 @@
 
 // Write your JavaScript code.
 
+//GLOBAL VARIABLES
+let transcribe = '';
+
+//CUSTOM FUNCTIONS
 function goBack() {
     window.history.back();
 }
 
-const myElement = document.getElementById("result");
-myElement.style.fontSize = "20px";
-
-$(document).ready(function () {
+$(function () {
+    //used to call the init() function after a successful load from the HTML.
     init();
-})
+});
 
 function init() {
     console.log("INIT IS HERE")
+
+    // Initialize the 'result' variable to store the captured speech
+
     const result = document.getElementById('result'); // Assuming you have an HTML element with the ID 'result'
     const toggle = document.getElementById('toggle'); // Assuming you have an HTML element with the ID 'toggle'
 
@@ -33,29 +38,28 @@ function init() {
         speech.recognition.addEventListener('result', (event) => {
             const audio = event.results[event.results.length - 1];
             speech.text = audio[0].transcript;
-            const tag = document.activeElement.nodeName;
-            console.log("TAG: " + tag);
-            if (tag === 'INPUT' || tag === 'TEXTAREA') {
-                if (audio.isFinal) {
-                    document.activeElement.value += speech.text;
-                }
-            }
             result.innerText = speech.text;
-            if (audio.isFinal)
-                populateAssessmentForms(speech.text);
+            if (audio.isFinal) {
+                //delayedMessage();
+                transcribe += speech.text;
+                console.log("TRANSCRIBE: \n" + transcribe + "\n");
+                populateAssessmentForms(transcribe);
+                //resetTranscribeAfterDelay();
+            }
         });
 
         toggle.addEventListener('click', () => {
             speech.listening = !speech.listening;
             if (speech.listening) {
-                toggle.classList.add('listening');
-                toggle.innerText = 'Listening ...';
+                //toggle.classList.add('listening');
+                //toggle.innerText = 'Listening ...';
                 speech.recognition.start();
             }
             else {
-                toggle.classList.remove('listening');
-                toggle.innerText = 'Toggle listening';
+                //toggle.classList.remove('listening');
+                //toggle.innerText = 'Toggle listening';
                 speech.recognition.stop();
+                transcribe = '';
             }
         })
     }
@@ -65,47 +69,40 @@ function init() {
         var el;
         var question = "";
         var answer = result;
+        var getId = "";
 
         if (typeof result !== "string") {
             throw new Error("valueResult must be a string.");
         }
 
+        //does the checking
         switch (true) {
             case result.includes("full name"):
                 question = "What is the patient's full name?";
-                el = document.getElementById("firstNameInput");
-                if (el.id == "firstNameInput") {
-                    el.value = await getPrompt(question, answer);
-                }
-                else {
-                    console.log('DOM.el is NULL')
-                }
+                getId = "inputPatientName";
+                el = document.getElementById(getId);
+                el.id == getId ? el.value = await getPrompt(question, answer) : console.log('DOM.el is NULL');
+                transcribe = '';
+                break;
+            case result.includes("health care worker") || result.includes("healthcare worker"):
+                question = "What is the health care worker's full name?";
+                getId = "inputDoctorName";
+                el = document.getElementById(getId);
+                el.id == getId ? el.value = await getPrompt(question, answer) : console.log('DOM.el is NULL');
+                transcribe = '';
+                break;
+            case (result.includes("medical conditions")) && (result.includes("this is noted")):
+                question = "What are the patient's medical conditions? Write it in a bullet form.";
+                getId = "inputMedicalConditions";
+                el = document.getElementById(getId);
+                el.id == getId ? el.value = await getPrompt(question, answer) : console.log('DOM.el is NULL');
+                transcribe = '';
                 break;
             default:
-                console.log('Invalid grade.');
+                console.log('end of swtich');
+                break;
         }
         return null;
-
-        //if (result.innerText.includes("first name")) {
-        //    var el = document.getElementById('firstNameInput');
-        //    el.value = speech.text;
-        //    //el.getElementsByTagName('input').value = speech.text;
-        //    //setFirstNameValue(speech.text)
-        //    //el.getElementsByTagName('input').value += speech.text;
-        //    //console.log("NAME: " + speech.text)
-        //}
-        //else if (result.innerText.includes("age")) {
-        //    var el = document.getElementById('Age');
-        //    el.getElementsByTagName('input')[0].value += speech.text;
-        //}
-        //else if (result.innerText.includes("sex")) {
-        //    var el = document.getElementById('Sex');
-        //    el.getElementsByTagName('input')[0].value = speech.text;
-        //}
-        //else if (result.innerText.includes("address")) {
-        //    var el = document.getElementById('Address');
-        //    el.getElementsByTagName('textarea')[0].value = speech.text;
-        //}
     }
 
     async function getPrompt(question, answer) {
@@ -127,4 +124,3 @@ function init() {
         }
     }
 }
-
