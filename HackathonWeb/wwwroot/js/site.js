14a5056145a64069d2fa72b329764e3ba81b8ee0
geraldine-s-sauz <geraldine.s.sauz@accenture.com>
2025-03-25 16:31:11 +0800

    Update site.js
    optimized keywords and timings

--- HackathonWeb/wwwroot/js/site.js
+++ HackathonWeb/wwwroot/js/site.js
@@ -1,10 +1,25 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
 
 //GLOBAL VARIABLES
 let transcribe = '';
+let fullTranscribe = '';
+let lastField = null;
+let listOfMentionField = new Set();
+
+
+const fieldKeywords = {
+    "Health Coordinator Name": "coordinator|health coordinator|doctor",
+    "Patient Full Name": "patient|full name",
+    "Allergies": "allergy|allergies",
+    "Medications": "medication|medications",
+    "Vaccine": "vaccine|vaccines",
+    "Medical History": "medical history|medical conditions|medical condition",
+    "Closing": "have a great day"
+};
+
 
 //CUSTOM FUNCTIONS
 function goBack() {
@@ -50,7 +65,8 @@ function init() {
                     transcribe = transcribe + " " + speech.text;
                     transcribe = transcribe.toLowerCase();
                     console.log("TRANSCRIBE: \n" + transcribe + "\n");
-                    fromWebkitSpeechRecognition(transcribe);
+                    fullTranscribe = fullTranscribe + " " + transcribe;
+                    fromWebkitSpeechRecognition(fullTranscribe);
                     //resetTranscribeAfterDelay();
                 }
             });
@@ -74,124 +90,118 @@ function init() {
         })
     }
 }
+async function fillFormFieldsBasedOnTranscript(transcript) {
+    for (const [key, value] of Object.entries(fieldKeywords)) {
+        if (containsAnyKeywords(transcript, value) && !listOfMentionField.has(key)) {
+            // Update the form field if a match is found
+            listOfMentionField.add(key);
+            if (lastField){
+                console.log(`Answering ${lastField}`);
+                await setFormField(lastField, transcript);
+            }
+            lastField = key
+        }
+    }
+}
+
+function containsAnyKeywords(text, keywords) {
+    if (!text) return false;
+    const regex = new RegExp(`\\b(${keywords})\\b`, "i"); // Case-insensitive whole word match
+    return regex.test(text);
+}
+
 
 //function to populate the assessment using webkitSpeechRecnognition.
-async function fromWebkitSpeechRecognition(result) {
-    console.log("fromWebkitSpeechRecognition is here!")
-    var el;
-    var question = "";
-    var answer = result;
-    var getId = "";
-
-    if (typeof result !== "string") {
-        throw new Error("valueResult must be a string.");
-    }
-
-    //var tempString = "Have you experienced any medical conditions today? Yeah, I've got hypertension and arthritis. I take amlodipine or amlodipine. I'm quite unsure. I'm a Lady Pin. That's right. That medication for my blood pressure and ibuprofen when my joints act up. And I also had a nephritis when I was young. Sporadic migraines pop up sometimes too, but I manage them with rest and Sumatriptan when needed. This is noted.";
-
-    //if (tempString.includes('medical conditions') && tempString.includes('this is noted')) {
-    //    console.log("Both 'medical conditions' and 'this is noted' are present in the string.");
-    //};
-
-    //does the checking
-    switch (true) {
-        case result.includes('full name') && result.includes('patient'):
-            question = "What is the patient's full name?";
-            getId = "inputPatientName";
-            el = document.getElementById(getId);
-            if (el.id == getId) {
-                el.value = await getAzureQnA(question);
-                transcribe = '';
+async function setFormField(fieldName, value) {
+    switch (fieldName) {
+        case "Health Coordinator Name":
+            if (!$("#inputDoctorName").val()) {
+                $("#inputDoctorName").val(await getAzureQnA2(
+                    "What is the Health Coordinator's full name?",
+                    value
+                ));
             }
             break;
-        case result.includes('health coordinator'):
-            question = "What is the Health Coordinator's full name?";
-            getId = "inputDoctorName";
-            el = document.getElementById(getId);
-            if (el.id == getId) {
-                el.value = await getAzureQnA(question);
-                transcribe = '';
+        case "Patient Full Name":
+            if (!$("#inputPatientName").val()) {
+                $("#inputPatientName").val(await getAzureQnA2(
+                    "What is the patient's full name?",
+                    value
+                ));
             }
             break;
-        case result.includes('allergies') && result.includes('noted'):
-            question = "Do you have allergies?"
-            getId = "inputAllergies";
-            el = document.getElementById(getId);
-            if (el.id == getId) {
-                el.value = await getAzureQnA(question);
-                transcribe = '';
+        case "Allergies":
+            if (!$("#inputAllergies").val()) {
+                $("#inputAllergies").val(await getPrompt(
+                    "Does the patient has allergies? If yes, list provide the list of allegies.",
+                    value
+                ));
             }
             break;
-        case (result.includes('medications') || result.includes('medication')) && result.includes('noted'):
-            question = "What are her medications?"
-            getId = "inputMedications";
-            el = document.getElementById(getId);
-            if (el.id == getId) {
-                el.value = await getAzureQnA(question);
-                transcribe = '';
+        case "Medications":
+            if (!$("#inputMedications").val()) {
+                $("#inputMedications").val(await getPrompt(
+                    "What are the medications of the patient, write in bullet form.",
+                    value
+                ));
             }
             break;
-        case result.includes('vaccine') && result.includes('noted'):
-            question = "Describe the patient's vaccine history in detail. If possible, write it in bullet form."
-            getId = "inputVaccine";
-            el = document.getElementById(getId);
-            if (el.id == getId) {
-                el.value = await getPrompt(question, answer);
-                transcribe = '';
+        case "Vaccine":
+            if (!$("#inputVaccine").val()) {
+                $("#inputVaccine").val(await getPrompt(
+                    "Describe the patient's vaccine history in detail. If possible, write it in bullet form.",
+                    value
+                ));
             }
             break;
-        case (result.includes('medical conditions') || result.includes('medical condition')) && result.includes('noted'):
-            question = "Describe the patient's medical history in detail. If possible, write it in bullet form."
-            getId = "inputMH";
-            el = document.getElementById(getId);
-            if (el.id == getId) {
-                el.value = await getPrompt(question, answer)
-                transcribe = '';
+        case "Medical History":
+            if (!$("#inputMH").val()) {
+                $("#inputMH").val(await getPrompt(
+                    "Describe the patient's medical history in detail. If possible, write it in bullet form.",
+                    value
+                ));
             }
             break;
-        default:
-            console.log('end of swtich');
+        case "Closing":
             break;
     }
 }
 
+//function to populate the assessment using webkitSpeechRecnognition.
+async function fromWebkitSpeechRecognition(result) {
+    console.log("fromWebkitSpeechRecognition is here!")
+
+    if (typeof result !== "string") {
+        throw new Error("valueResult must be a string.");
+    }
+    fillFormFieldsBasedOnTranscript(transcribe)
+    
+}
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 //function to complete the assessment
 async function completeAssessment() {
-    var el;
-    var question = "";
-    var answer = result;
-    var getId = "";
-
-    //testing123
-
-    switch (true) {
-        case true:
-            question = "Summarize the whole conversation between the health coordinator and patient. If possible, write the key points.";
-            getId = "inputSummary";
-            el = document.getElementById(getId);
-            if (el.id == getId) {
-                el.value = await getPromptFromTranscription(question)
-                transcribe = '';
-                break;
-            };
-        default:
-            console.log('end of swtich');
-            break;
-    }
+        $("#inputSummary").val(await getPrompt(
+            "Summarize the whole conversation between the health coordinator and patient, include key points like vaccine history, medication, medical conditions.",
+            transcribe
+        ));
 }
 
 //function to call OpenAI based on question and answer.
 async function getPrompt(question, answer) {
     console.log("I am in getPrompt");
+    console.log("Question: " + question)
 
     if (typeof question !== "string" || typeof answer !== "string") {
         throw new Error("value must be a string.");
     }
     try {
-        const response = await fetch(`https://localhost:44337/api/OpenAI/GetPrompt?Question=${encodeURIComponent(question)}&Answer=${encodeURIComponent(answer)}`);
+        const response = await fetch(`https://localhost:7290/api/OpenAI/GetPrompt?Question=${encodeURIComponent(question)}&Answer=${encodeURIComponent(answer)}`);
 
         if (response.ok) {
-            const data = await response.text(); // Parse the response as text
+            let data = await response.text(); // Parse the response as text
+            data = data.replace(/^\s+/, ''); // Remove leading whitespace
             console.log("Response received: ", data);
             return data; // Return the parsed data
         } else {
@@ -205,16 +215,18 @@ async function getPrompt(question, answer) {
 }
 
 //function to call OpenAI based on question and sample transcript
-async function getPromptFromTranscription(question) {
+async function getPromptFromTranscription(question, fullconversation) {
     if (typeof question !== "string") {
         throw new Error("valueResult must be a string.");
     }
     console.log("COMPLETING THE ASSESSMENT NOW");
     try {
-        const completeAssessmentResponse = await fetch(`https://localhost:44337/api/OpenAI/GetPromptFromTranscript?Question=${encodeURIComponent(question)}`);
+        //const completeAssessmentResponse = await fetch(`https://localhost:7290/api/OpenAI/GetPromptFromTranscript?Question=${encodeURIComponent(question)}`);
+        const completeAssessmentResponse = await fetch(`https://localhost:7290/api/OpenAI/GetPromptFromTranscript2?Question=${encodeURIComponent(question)}&Transcript=${encodeURIComponent(fullconversation)}`);
 
         if (completeAssessmentResponse.ok) {
-            const data = await completeAssessmentResponse.text(); // Parse the response as text
+            let data = await completeAssessmentResponse.text(); // Parse the response as text
+            data = data.replace(/^\s+/, ''); 
             console.log("Response received: ", data);
             return data; // Return the parsed data
         } else {
@@ -231,7 +243,26 @@ async function getPromptFromTranscription(question) {
 async function getAzureQnA(question) {
     console.log("I am in get AzureQnA");
     try {
-        const response = await fetch(`https://localhost:44337/api/QuestionAnswer/GetAzureQnA?QuestionUI=${encodeURIComponent(question)}`);
+        const response = await fetch(`https://localhost:7290/api/QuestionAnswer/GetAzureQnA?QuestionUI=${encodeURIComponent(question)}`);
+
+        if (response.ok) {
+            const data = await response.text(); // Parse the response as text
+            console.log("Response received from AzureQnA: ", data);
+            return data; // Return the parsed data
+        } else {
+            // Handle the error here
+            document.getElementById('output').innerText = 'Error occurred during the API call.';
+        }
+    } catch (error) {
+        // Handle any network or other errors here
+        document.getElementById('output').innerText = 'An error occurred: ' + error.message;
+    }
+}
+
+async function getAzureQnA2(question,transcribe) {
+    console.log("I am in get AzureQnA2");
+    try {
+        const response = await fetch(`https://localhost:7290/api/QuestionAnswer/GetAzureQnA2?QuestionUI=${encodeURIComponent(question)}&TranscribeText=${encodeURIComponent(transcribe)}`);
 
         if (response.ok) {
             const data = await response.text(); // Parse the response as text
@@ -250,7 +281,7 @@ async function getAzureQnA(question) {
 //function to call Azure microphone
 async function getFromMicrophone() {
     try {
-        const response = await fetch(`https://localhost:44337/api/QuestionAnswer?QuestionUI=${encodeURIComponent(question)}`);
+        const response = await fetch(`https://localhost:7290/api/QuestionAnswer?QuestionUI=${encodeURIComponent(question)}`);
 
         if (response.ok) {
             const data = await response.text(); // Parse the response as text
